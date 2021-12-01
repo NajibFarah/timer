eb0d2c6a76a1abfb584928d7a924c6178447769d
Najib <muhammed.najib.farah@gmail.com>
2021-12-01 01:48:58 +0000

    sound timer

--- timer1.js
+++ timer1.js
@@ -1,12 +1,12 @@
 var count = 1000;
-    var alarms = process.argv;
-     const timer = () => {
-      for (i = 2; i < alarms.length; i++) {
-    if (alarms[i] >= 0 && alarms[i] !== NaN) {
-      setTimeout(() => {
-        process.stdout.write('\x07');
-      }, count * alarms[i])
-    }
-  }
+var alarms = process.argv;
+ const timer = () => {
+  for (i = 2; i < alarms.length; i++) {
+ if (alarms[i] >= 0 && alarms[i] !== NaN) {
+  setTimeout(() => {
+    process.stdout.write('\x07');
+  }, count * alarms[i])
+ }
+}
 };   
 timer(alarms); 
